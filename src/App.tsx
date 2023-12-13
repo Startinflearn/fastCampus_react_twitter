9ccb48eaa072b3e04be153057070bf3bb2111f1e
Raon98 <cheol1998@kakao.com>
2023-12-13 18:46:47 +0900

    react-router-dom을 이용한 라우팅 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+    <Routes>
+        <Route path='/' element={<h1>Home page</h1>}/>
+        <Route path='/posts' element={<h1>PostList page</h1>}/>
+        <Route path='/posts/:id' element={<h1>Post Detail page</h1>}/>
+        <Route path='/posts/new' element={<h1>Post new page</h1>}/>
+        <Route path='/posts/edit/:id' element={<h1>Post Edit page</h1>}/>
+        <Route path='/profile' element={<h1>Profile page</h1>}/>
+        <Route path='/profile/edit' element={<h1>Profile Edit page</h1>}/>
+        <Route path='/notification' element={<h1>Notification page</h1>}/>
+        <Route path='/search' element={<h1>Search page</h1>}/>
+        <Route path='/users/login' element={<h1>Login page</h1>}/>
+        <Route path='/users/signup' element={<h1>Signup page</h1>}/>
+        <Route path='*' element={<Navigate replace to="/"/>}/>
+    </Routes>
+    )
 }
 
 export default App;
